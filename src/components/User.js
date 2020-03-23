39f2ceb652c68e85a0b3742059a5cd5985f451c6
Qi Cao <alanqicao@gmail.com>
2020-03-22 21:27:28 -0600

    User

--- src/components/User.js
+++ src/components/User.js
@@ -1,18 +1,23 @@
 import React from "react";
 import {Card,} from "semantic-ui-react";
+import {UserConsumer,} from "../providers/UserProvider"
 
 const User =()=>(
+  <UserConsumer>
+    {value => (
   <Card>
     <Card.Content>
-      <Card.Header>User</Card.Header>
+      <Card.Header>{value.firstName}</Card.Header>
       <Card.Meta>
-        Date Joined: dateJoined
+        Date Joined: {value.dateJoined}
       </Card.Meta>
     </Card.Content>
     <Card.Content>
-      <p>User Avatar:menmbershipLevel </p>
+      <p>User Avatar:{value.avatar}</p>
     </Card.Content>
   </Card>
+  )}
+  </UserConsumer>
 )
 
 export default User;
